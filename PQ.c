b1cdcc069a62c8fe9e1d842a871f9f292f35f38b
Erik Rodriguez <earodriguez1996@gmail.com>
2021-02-20 17:31:22 -0500

    Added more functionality to PQ and commented it

--- PQ.c
+++ PQ.c
@@ -39,11 +39,13 @@ priQue* initPriorityQueue(){
     cpuEvent* start = malloc(sizeof(cpuEvent));
     cpuEvent* end = malloc(sizeof(cpuEvent));
 
+    //simple malloc check, used moreso for debugging
     if(heap == NULL || start == NULL || end == NULL){
         printf("Memory allocation failure");
         exit(1);
     }
 
+    //These are the values that the heap and events get initialized with
     heap -> max = 10;
     heap -> size = 2;
 
@@ -61,11 +63,82 @@ priQue* initPriorityQueue(){
     return heap;    
 }
 
-int cmp(const void *x, const void *y){
+//time comparison/difference function
+int compare(const void *x, const void *y){
 
     cpuEvent* a = (cpuEvent*) x;
     cpuEvent* b = (cpuEvent*) y;
     return (a -> time) - (b -> time)
 }
+
+//Returns 1 if queue is full, 0 if not
+int isFull(priQueue* heap){
+
+    if(heap -> max == heap -> size){
+    return 1;
+    }
+    return 0;
+}
+
+//Pushes an event to the PQ, PQ takes its time and sets it as its own to "advance" time
+//qsort is an array sorting function, used to sort the heap after changes are made
+void pushToQue(priQue* heap, cpuEvent* eve){
+
+    if(isFull(heap)){
+        return;
+    }
+
+    int time = eve -> time;
+    heap -> size += 1;
+    heap -> event[heap -> size - 1] = *eve;
+    qsort(heap -> event, heap -> size, sizeof(cpuEvent), compare);
+}
+
+//Pops the top element in the heap, IE removes it
+cpuEvent* popEve(priQue* heap){
+
+    cpuEvent* rm = heap -> event
+    heap -> event = &heap -> event[1];
+    heap -> size -= 1;
+    return rm;
+}
+
+//Function to clear the whole PQ, used moreso for debugging
+void clearPriQue(priQue* heap){
+    free(heap);
+}
+
+//Simple check to see if the heap is empty (0 indicates empty)
+int isEmpty(priQue* heap){
+
+    return heap -> size;
+}
+
+//Prints the current queue event
+void printQue(priQue* heap){
+
+    for(int i = 0; i < heap -> size; i++){
+    printf("JOB-SEQ: %-2d, TIME:%-5d type:%-2d\n", heap -> event -> jobSeq, heap -> event -> time,
+                                                   heap -> event -> type);
+    heap -> event++;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
